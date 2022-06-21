db3dbdcf1f29b0b5c98d6109e80f03954edcea64
cbirchy87 <cbirchy87@gmail.com>
2022-06-21 11:57:36 +0100

    Update API URL
    Changed the URL to use AWS

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'http://quizapicb-env.eba-m28fmepb.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
