6ed09c9c956e658c24364c56bfac655e7898884c
Matthew-Place <matt.1504@hotmail.co.uk>
2022-09-21 13:59:23 +0100

    added https://cors-everywhere.herokuapp.com/http://

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'https://cors-everywhere.quiz-api-mpt.eu-west-2.elasticbeanstalk.com/';
+    service.apiBase = 'https://cors-everywhere.herokuapp.com/http://quiz-api-mpt.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
