90e91d6fb26b596541302484e40eaecf28fedec5
Matthew-Place <matt.1504@hotmail.co.uk>
2022-09-21 13:53:43 +0100

    added cors-everywhere

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://quiz-api-mpt.eu-west-2.elasticbeanstalk.com/';
+    service.apiBase = 'https://cors-everywhere.quiz-api-mpt.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
