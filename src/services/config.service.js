fc75afb778a275876b1da942471bae1de17983dd
OliviaLB <olivia.l.bates@gmail.com>
2024-04-03 12:01:35 +0100

    Update url to point at cloud service

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://ob-quiz-app-env.eba-natpkg53.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
